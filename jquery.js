c600ba41af57371ad7603c5477cba1b63bd55dc6
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-02-07 16:58:03 +0530

    Added animations to skills page floating the cards is still remaining

--- jquery.js
+++ jquery.js
@@ -1,25 +1,43 @@
 // Feedback from animation
 
 {
+  $(document).ready(function () {
+    //Feedback form heading and details
+    $("h1").click(function () {
+      $("#details").toggle(1000);
+    });
 
-$(document).ready(function () {
-  
-    $('h1').click(function(){
-              $('#details').toggle(1000)
-           })
-  
- 
-  
+    //Non technical skills animation
+    $(".skill").mouseleave(function () {
+      $(".skill-details").hide(1000);
+      $(".skill-details").css('display', 'block');
+    });
+
+    $(".skill").mouseenter(function () {
+      $(".skill-details").show(1000);
+      $(".skill-details").css('display', 'block');
+      
+    });
+
+    $(".skill2").mouseleave(function () {
+      $(".skill-details2").hide(1000);
+      $(".skill-details2").css('display', 'block');
+    });
+    $(".skill2").mouseenter(function () {
+      $(".skill-details2").show(1000);
+      $(".skill-details2").css('display', 'block');
+    });
+
     //Animate Function in Jquery
-    $('#lorem').animate({
-        opacity:0.3,
-        height : '150px',
-        width :'350px'
-    },1000)
-    $('#lorem').animate({
-        opacity:0.9,
-        height : '550px',
-        width :'1050px'
-    },1000)
+    // $('#lorem').animate({
+    //     opacity:0.3,
+    //     height : '150px',
+    //     width :'350px'
+    // },1000)
+    // $('#lorem').animate({
+    //     opacity:0.9,
+    //     height : '550px',
+    //     width :'1050px'
+    // },1000)
   });
 }
