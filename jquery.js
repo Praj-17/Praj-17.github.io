0f898f3555830e0434f766c15626f0685584d613
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-04-29 15:45:18 +0530

    added s4ds inaugration day

--- jquery.js
+++ jquery.js
@@ -48,3 +48,15 @@
     // },1000)
   });
 }
+$(document).ready(function() {
+  $("#bookmarkme").click(function() {
+    if (window.sidebar) { // Mozilla Firefox Bookmark
+      window.sidebar.addPanel(location.href,document.title,"");
+    } else if(window.external) { // IE Favorite
+      window.external.AddFavorite(location.href,document.title); }
+    else if(window.opera && window.print) { // Opera Hotlist
+      this.title=document.title;
+      return true;
+    }
+  });
+});
